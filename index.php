5624e6aff3c9308c497c74b4131768d80cf71d6a
Chema Zumalacárregui <chema.zumalacarreguirbootcamp-upgrade.com>
2024-04-09 15:05:35 +0200

    muestra raid twit

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php
-$texto = "En un lugar de la Mancha...";
-$autor = "Lola";
-$usuario="@lola";
-
+$twits = [
+    [
+        "texto" => "En un lugar de la Mancha...",
+        "autor" => "Lola",
+        "usuario" => "@lola"
+    ],
+    [
+        "texto" => "En un lugar de la Mancha...",
+        "autor" => "Pedro",
+        "usuario" => "@Pedro"
+    ],
+    [
+        "texto" => "En un lugar de la Mancha...",
+        "autor" => "Charly",
+        "usuario" => "@Charly"
+    ]
+];
 
 require_once("index.html");
